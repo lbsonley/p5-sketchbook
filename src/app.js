ed3b10233ab50643bc381f4dc20b48013b7b34b3
Benjamin Sonley <benjamin.sonley@unic.com>
2019-11-28 21:49:27 +0100

    Clean up dynamic rendering router code

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ import page from "page";
 import { init, routes, render } from "./routes";
 import sum from "./scripts/index.js";
 
+// Middleware for initializing/resetting routing context
 page("*", init.ctx);
+
+// Routes
 page("/", "/sketchbook");
 page("/sketchbook", routes.sketchbook);
-page("/sketchbook/:sketch", routes.sketchbook);
+page("/sketchbook/:sketch", routes.sketch);
 
+// Middleware for compiling templates and updating DOM
 page("*", render.content);
 page("/sketchbook/:sketch", render.sketch);
+page("/sketchbook/:sketch", render.activeLink);
+page("/sketchbook/:sketch", render.code);
+
+// Init Page.js
 page();
--- src/routes/index.js
+++ src/routes/index.js
@@ -13,46 +13,82 @@ export const init = {
   }
 };
 
+/**
+ * Define context and templates for each route
+ */
 export const routes = {
   sketchbook: function(ctx, next) {
     ctx.partials.content = sidebar;
     ctx.data.title = "Sketchbook";
-    // ctx.data.pageContent =
-    //   "It snowed and snowed and snowed until we could barely see out the windows. So we climbed on the roof, strapped in to our floatiest boards and enjoyed the deep powder snow.";
+    ctx.data.pageContent =
+      "It snowed and snowed and snowed until we could barely see out the windows. So we climbed on the roof, strapped in to our floatiest boards and enjoyed the deep powder snow.";
+    ctx.data.sidebarLinks = sidebarLinks;
+    next();
+  },
+  sketch: function(ctx, next) {
+    ctx.partials.content = sidebar;
+    ctx.data.title = "Sketchbook";
     ctx.data.sidebarLinks = sidebarLinks;
     next();
   }
 };
 
+/**
+ * DOM Manipulation in order to display page content
+ */
 export const render = {
+  /**
+   * @description
+   */
   content: function(ctx, next) {
     const loading = document.querySelector("#loading");
     const el = document.querySelector("#content");
+
     const template = Hogan.compile(ctx.partials.content);
     const content = template.render(ctx.data, ctx.partials);
     el.innerHTML = content;
     loading.style.display = "none";
 
     // if we are viewing a sketch, go on to render the sketch
-    if (ctx.params.sketch) next();
+    if (ctx.params.sketch) {
+      next();
+    }
   },
-  sketch: function(ctx) {
+  /**
+   * @description
+   */
+  sketch: function(ctx, next) {
     const sketch = ctx.params.sketch;
     const sketchContainer = document.querySelector("#sketch");
-    const codeContainer = document.querySelector("#code");
+
+    sketchContainer.innerHTML = "";
+    new p5(sketches[sketch].module, "sketch");
+
+    next(sketch);
+  },
+  /**
+   * @description
+   */
+  activeLink: function(ctx, next) {
+    const sketch = ctx.params.sketch;
     const allLinks = document.querySelectorAll("a");
     const activeLink = document.querySelector(
       `a[href="/sketchbook/${sketch}"]`
     );
 
     allLinks.forEach(link => link.classList.remove("active"));
-    activeLink.classList.add("active");
+    if (activeLink) activeLink.classList.add("active");
 
-    sketchContainer.innerHTML = "";
-    new p5(sketches[sketch].module, "sketch");
-
+    next();
+  },
+  /**
+   * @description
+   */
+  code: function(ctx) {
+    const sketch = ctx.params.sketch;
+    const codeContainer = document.querySelector("#code");
     codeContainer.innerHTML = "";
-    codeContainer.innerHTML = sketches[sketch].code.trim();
+    codeContainer.innerHTML = sketches[sketch].code;
     Prism.highlightAll();
   }
 };
